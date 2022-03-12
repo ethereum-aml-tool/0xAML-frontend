c890a3369fb2aa70b3ba1106efa48bc18e56b3c7
Pontus Backman <ponbac@student.chalmers.se>
2022-03-12 08:55:41 +0100

    added public api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
-export const API_URL: string = "http://127.0.0.1:8000";
+//export const API_URL: string = "http://127.0.0.1:8000";
+export const API_URL: string = "http://95.80.29.243:8000";
 export const EXPLORER_URL: string = "https://etherscan.io";
