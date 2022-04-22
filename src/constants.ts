f19b5ba8565407ea9e663bf22e0effbe30433cc5
ponbac <root@ponbac.xyz>
2022-04-22 22:17:45 +0200

    changed to public ip

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL: string = "http://127.0.0.1:1444";
+export const API_URL: string = "http://98.128.217.173:1444";
 //export const API_URL: string = "https://zero-x-aml.herokuapp.com";
 export const EXPLORER_URL: string = "https://etherscan.io";
 
