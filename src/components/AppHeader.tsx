4c73a18a3a911f7f569c6ff219e9bcda9bb4a121
ponbac <root@ponbac.xyz>
2023-05-23 23:29:21 +0200

    upd image

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -5,12 +5,12 @@ const AppHeader = () => {
   return (
     <>
       <img
-        src={logo}
-        className="animate-speed h-60 motion-safe:animate-spin"
+        src="/indago-main.png"
+        className="animate-speed h-96"
         style={{ animationDuration: "20s" }}
         alt="logo"
       />
-      <p className="mt-14 bg-gradient-to-r from-tornado-green to-green-600 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
+      <p className="z-50 -mt-16 bg-gradient-to-r from-tornado-green to-green-600 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
         <span className="font-mono italic">{APP_NAME.toUpperCase()}</span>
       </p>
       <p className="bg-gradient-to-r from-tornado-green to-green-600 bg-clip-text text-xl font-black text-transparent selection:bg-transparent">
